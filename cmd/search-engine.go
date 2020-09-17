16b7e55cd1003a4e8d2fb6f44df907db8014c712
zanefinner <zanefinnergm@gmail.com>
2020-09-17 15:36:44 -0400

    go to partial routes

--- cmd/search-engine.go
+++ cmd/search-engine.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/zanefinner-projects/search-engine/pkg/crawler"
@@ -23,9 +25,13 @@ func main() {
 }
 func root(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, `<!DOCTYPE html>`)
+	io.WriteString(w, `
+	<form action="/search" method="GET">
+	<input name="q"></input>
+	<button type="submit">Go!</button>`)
 }
 func search(w http.ResponseWriter, r *http.Request) {
-
+	start := time.Now()
 	defer log.Println("[Root] <- [Client]")
 	io.WriteString(w, "<!DOCTYPE html>")
 	q, ok := r.URL.Query()["q"]
@@ -36,10 +42,19 @@ func search(w http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Println("searching", q[0])
 		content, links := crawler.Crawl(q[0], 77)
+		log.Println("Retrieved in", "[", q[0], "]", time.Since(start))
 		io.WriteString(w, content)
 		for _, link := range links {
+			linkSlc := strings.Split(link, "")
+			if len(linkSlc) != 0 {
 
-			io.WriteString(w, "<a href='http://localhost:8765/search?q="+link+"'>"+link+"</a></br>")
+				if linkSlc := strings.Split(link, ""); linkSlc[0] != "/" {
+					io.WriteString(w, "<a href='http://localhost:8765/search?q="+link+"'>"+link+"</a></br>")
+				} else {
+
+					io.WriteString(w, "<a href='http://localhost:8765/search?q="+q[0]+link+"'>"+link+"</a></br>")
+				}
+			}
 		}
 	}
 }

e5ce5511695e308eb32aad7baa3e8eb4cb45abda
zanefinner <zanefinnergm@gmail.com>
2020-09-18 19:02:41 -0400

    plan

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	//Crawl assets/sites
+	//Crawl all 5 letter combinations
+	//Use all links with robots.txt in mind ^
+	//Create new go routine
+	////Add to db using titles/metas
+	////further crawl
+	////further add
+	//Serve search page
+	//Search through db and deliver
+}
